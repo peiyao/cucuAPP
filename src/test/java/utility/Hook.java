2d0d91550a83273ff7b8b63707c6f72ec33da65d
peiyao <zhangpeiyao19861108@gmail.com>
2018-10-08 17:16:53 +0200

    Update Hook.java

--- src/test/java/utility/Hook.java
+++ src/test/java/utility/Hook.java
@@ -3,29 +3,34 @@
  */
 package utility;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.Scenario;
+import cucumber.api.testng.CucumberFeatureWrapper;
+import cucumber.api.testng.TestNGCucumberRunner;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-/**
- * @author CHIRAG
- *
- */
 public class Hook {
 
 	private static WebDriver driver;
-	
+	private TestNGCucumberRunner testNGCucumberRunner;
+	@BeforeClass()
+	public void setUpClass() throws Exception {
+		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
+	}
 	@Before("@web")
 	public void setUp()
 	{
@@ -40,12 +45,35 @@ public class Hook {
 	{
 		DesiredCapabilities cap = new DesiredCapabilities();
 		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "Nexus 9");
+		cap.setCapability(MobileCapabilityType.DEVICE_NAME, "BH905CXJ5Z");
 		cap.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"//App//ApiDemos.apk");
+
+		//cap.setCapability(MobileCapabilityType.APP, System.getProperty("user.dir")+"//App//Rebtel.apk");
+		//cap.setCapability("appActivity", "com.tuenti.messenger.ui.activity.MainActivity");
+		//cap.setCapability("appWaitPackage", "com.rebtel.android.client.RebtelAppApplication");
+		//cap.setCapability("appWaitActivity", "com.rebtel.android.client.RebtelActivity");
 		driver = new AndroidDriver<WebElement>(new URL("http://0.0.0.0:4723/wd/hub"), cap);
 		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
-	
+	@Before
+	public void initializeTest(){
+		// Code to setup initial configurations
+	}
+
+
+	@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
+	public void feature(CucumberFeatureWrapper cucumberFeature) {
+		testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
+	}
+	@DataProvider
+	public Object[][] features() {
+		return testNGCucumberRunner.provideFeatures();
+	}
+
+	@AfterClass(alwaysRun = true)
+	public void tearDownClass() throws Exception {
+		testNGCucumberRunner.finish();
+	}
 	@After
 	public void tearDown()
 	{
@@ -55,5 +83,15 @@ public class Hook {
 	public static WebDriver getDriver()
 	{
 		return driver;
+	}
+
+	public void embedScreenshot(Scenario scenario) {
+		if (scenario.isFailed()) {
+			try {
+				// Code to capture and embed images in test reports (if scenario fails)
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
