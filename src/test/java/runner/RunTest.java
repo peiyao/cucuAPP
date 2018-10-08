a4374cd5dd21e0981f9001a303ad097b64c7e631
peiyao <zhangpeiyao19861108@gmail.com>
2018-10-08 17:15:47 +0200

    Update RunTest.java

--- src/test/java/runner/RunTest.java
+++ src/test/java/runner/RunTest.java
@@ -1,23 +1,23 @@
 package runner;
 
-import org.testng.annotations.Test;
 import cucumber.api.CucumberOptions;
+
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-
-/**
- * @author CHIRAG
- *
- */
+import org.testng.annotations.Test;
 
 // Uncomment @RunWith if you are using Junit to run Test 
-// @RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 
 @CucumberOptions(features={"src//test//java//features"}
 					,glue={"stepdefinations","utility"}
-					,plugin = {"pretty", "html:target/cucumber"}
-					, tags ={"@web"}
-		)
+					,plugin = {"pretty"}
+					, tags ={"@appium"}
+					,format = {"json:target/json-report/dw.json",
+                               "html:target/cucumber-reports/cucumber-pretty",
+                               "rerun:target/cucumber-reports/rerun.txt"
+                })
+
 @Test
 public class RunTest extends AbstractTestNGCucumberTests{
 
